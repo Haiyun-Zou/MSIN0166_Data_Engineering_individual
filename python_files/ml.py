69279330a33c0bc5ecd1bb88a6d2fc5dd84e00ed
Haiyun Zou <haiyun.zou.21@ucl.ac.uk>
2022-04-25 10:17:26 +0000

    ml spark transfomed

--- python_files/ml.py
+++ python_files/ml.py
@@ -8,7 +8,6 @@ import requests
 from datetime import datetime
 from IPython.display import Image
 import matplotlib.pyplot as plt
-np.random.seed(42)
 
 # set up the spark envrionment
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
@@ -27,8 +26,8 @@ dummies_df = pd.get_dummies(players['pos'], prefix='is')
 
 # add the columns into the data frame
 for c in dummies_df.columns:
-    players[c] = dummies_df[c]
-
+    players[c] = list(dummies_df[c])
+    
 # convert from True/False to 1/0
 players['in_2021_22_season'] = players['in_2021_22_season'].replace({True:1, False:0})
 
@@ -38,134 +37,196 @@ players = players.sort_values('name')
 players = players.drop([ 'team', 'name', 'link', 'pos', 'No.'], axis = 1)
 
 import yaml
-from sklearn.model_selection import train_test_split
 # load the yaml document
 params = yaml.safe_load(open("/project/MSIN0166_Data_Engineering_individual/params.yaml"))["ML"]
 
+print(params)
+
+# create spark df
+players_df  = spark.createDataFrame(players)
+
 # split the train and test sets
-players_train, players_test = train_test_split(players, test_size = params['split'], shuffle = params['shuffle'], random_state = params['seed'])
+players_train, players_test  = players_df.randomSplit([1-params['split'],params['split']],seed=params['seed'])
 
-# extract the columns for transformation
-players_train_trans = pd.DataFrame(players_train[players_train.columns[9:-5]])
-players_test_trans = pd.DataFrame(players_test[players_test.columns[9:-5]])
-X_train = np.array(players_train_trans)
-X_test = np.array(players_test_trans)
+# select columns for pca
+players_train_trans = players_train.select(players_train.columns[9:-5])
+players_test_trans = players_test.select(players_test.columns[9:-5])
 
-from sklearn.preprocessing import StandardScaler
-# standardise the data
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.fit_transform(X_test)
+print(players_train_trans.toPandas().shape)
 
-from sklearn.decomposition import PCA
-# get the first 10 pca components
-pca_train = PCA(n_components=params['n_components'])
-pca_test = PCA(n_components = params ['n_components'])
-pca_train.fit(X_train)
-pca_test.fit(X_test)
-players_pca_train= pca_train.transform(X_train)
-players_pca_test= pca_test.transform(X_test)
+from pyspark.ml.feature import StandardScaler
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler
 
-sum(pca_train.explained_variance_ratio_)
+# assemble the training data first
+assemblers = [VectorAssembler(inputCols=[col], outputCol=col + "_vec") for col in players_train_trans.columns]
+# standerdise the data
+scaler = [StandardScaler(inputCol=col + "_vec", outputCol=col + "_scaled") for col in players_train_trans.columns]
+pipeline = Pipeline(stages= assemblers + scaler)
+X_train = pipeline.fit(players_train_trans)
+X_train = X_train.transform(players_train_trans)
 
+
+# assemble the testing data
+assemblers = [VectorAssembler(inputCols=[col], outputCol=col + "_vec") for col in players_test_trans.columns]
+# standerdise the data
+scaler = [StandardScaler(inputCol=col + "_vec", outputCol=col + "_scaled") for col in players_test_trans.columns]
+pipeline = Pipeline(stages= assemblers + scaler)
+X_test = pipeline.fit(players_test_trans)
+X_test = X_test.transform(players_test_trans)
+
+# get the scaled columns
+columns = [i for i in X_train.columns if 'scaled' in i]
+
+from pyspark.ml.feature import PCA
+# assemble the columns into features column
+assemblers = VectorAssembler(inputCols= ['G_2122_scaled',
+ 'GS_2122_scaled',
+ 'MP_2122_scaled',
+ 'FG%_2122_scaled',
+ '3P%_2122_scaled',
+ '2P%_2122_scaled',
+ 'eFG%_2122_scaled',
+ 'FT%_2122_scaled',
+ 'ORB_2122_scaled',
+ 'DRB_2122_scaled',
+ 'AST_2122_scaled',
+ 'STL_2122_scaled',
+ 'BLK_2122_scaled',
+ 'TOV_2122_scaled',
+ 'PF_2122_scaled',
+ 'PTS_2122_scaled',
+ 'G_career_scaled',
+ 'GS_career_scaled',
+ 'MP_career_scaled',
+ 'FG%_career_scaled',
+ '3P%_career_scaled',
+ '2P%_career_scaled',
+ 'eFG%_career_scaled',
+ 'FT%_career_scaled',
+ 'ORB_career_scaled',
+ 'DRB_career_scaled',
+ 'AST_career_scaled',
+ 'STL_career_scaled',
+ 'BLK_career_scaled',
+ 'TOV_career_scaled',
+ 'PF_career_scaled',
+ 'PTS_career_scaled'], outputCol="features")
+
+# transform the training data and return the PCA result in the column of PCA_features
+X_train_v = assemblers.transform(X_train)
+PCA_train = PCA(k = 10, inputCol="features", outputCol = 'PCA_features')
+
+X_train_model = PCA_train.fit(X_train_v)
+X_train = X_train_model.transform(X_train_v)
+
+# transform the testing data and return the PCA result in the column of PCA_features
+X_test_v = assemblers.transform(X_test)
+PCA_test = PCA(k = 10, inputCol="features", outputCol = 'PCA_features')
+
+X_test_model = PCA_test.fit(X_test_v)
+X_test = X_test_model.transform(X_test_v)
+
+print(X_train.toPandas().shape)
+
+print(sum(X_train_model.explainedVariance))
 
 # plot the PCA variation
 plt.figure(figsize = (8,5))
-plt.plot(np.cumsum(pca_train.explained_variance_ratio_),linestyle='solid', marker='o')
+plt.plot(np.cumsum(X_train_model.explainedVariance),linestyle='solid', marker='o')
 plt.xlabel('Number of Components')
 plt.ylabel('Cumulative Explained Variance')
 plt.title('Variation explained by PCA')
 
 plt.show()
 
-# convert the pca result into data frame
-pca_players_train_df = pd.DataFrame(players_pca_train)
-pca_players_test_df = pd.DataFrame(players_pca_test)
+# get the nonPCA columns
+players_train_first = players_train.select(list(set(players_train.columns[:9]) | set(players_train.columns[-5:])))
+players_test_first = players_test.select(list(set(players_test.columns[:9]) | set(players_test.columns[-5:])))
 
-# rename the columns
-pca_players_train_df.columns = [f'PCA{i+1}' for i in pca_players_train_df.columns]
-pca_players_test_df.columns = [f'PCA{i+1}' for i in pca_players_test_df.columns]
+# convert the PCA result column into data frame
+temp_train = X_train.select('PCA_features').rdd.map(lambda x: [float(i) for i in x['PCA_features']]).toDF(['PCA' + str(i+1) for i in range(10)])
+temp_test = X_test.select('PCA_features').rdd.map(lambda x: [float(i) for i in x['PCA_features']]).toDF(['PCA' + str(i+1) for i in range(10)])
 
-# select the variables for building the mdel
-players_train = pd.DataFrame(players_train[list(set(players_train.columns[:9]) | set(players_train.columns[-5:]))])
-players_test = pd.DataFrame(players_test[list(set(players_test.columns[:9]) | set(players_test.columns[-5:]))])
+# convert back to pandas
+players_train_first_pd = players_train_first.toPandas()
+players_test_first_pd = players_test_first.toPandas()
+temp_train_pd = temp_train.toPandas()
+temp_test_pd = temp_test.toPandas()
 
-# add the pca result columns into the data frame
-for c in pca_players_train_df.columns:
-    players_train[c] = list(pca_players_train_df[c])
-for c in pca_players_test_df.columns:
-    players_test[c] = list(pca_players_test_df[c])
+# merge the columns together
+for c in temp_train_pd.columns:
+    players_train_first_pd[c] = temp_train_pd[c]
+for c in temp_test_pd.columns:
+    players_test_first_pd[c] = temp_test_pd[c]
+    
+# convert back to spark df
+players_train_final = spark.createDataFrame(players_train_first_pd)
+players_test_final = spark.createDataFrame(players_test_first_pd)
 
-# get the X variables
-X_train = players_train.drop('playoff', axis = 1)
-X_test = players_test.drop('playoff', axis = 1)
+from pyspark.ml.classification import DecisionTreeClassifier
 
-# get the y variables
-y_train = players_train['playoff'].astype(np.int64)
-y_test = players_test['playoff'].astype(np.int64)
+# assemble all the X columns into features
+assembler = VectorAssembler(inputCols =['is_PG',
+ 'is_SF',
+ 'is_SG',
+ 'is_eastern',
+ 'height',
+ 'guaranteed',
+ 'is_PF',
+ 'age',
+ 'birth',
+ 'is_C',
+ 'weight',
+ 'in_2021_22_season',
+ 'exp',
+ 'PCA1',
+ 'PCA2',
+ 'PCA3',
+ 'PCA4',
+ 'PCA5',
+ 'PCA6',
+ 'PCA7',
+ 'PCA8',
+ 'PCA9',
+ 'PCA10'] , outputCol='features')
 
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+# transform the train and test data
+players_train_final = assembler.transform(players_train_final)
+players_test_final= assembler.transform(players_test_final)
 
-import random
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import classification_report
-from sklearn.ensemble import AdaBoostClassifier
+# rename the column playoff to label
+players_train_final = players_train_final.withColumnRenamed('playoff', 'label')
+players_test_final= players_test_final.withColumnRenamed('playoff', 'label')
 
-# get default decision tree classifier
-clf_dt = DecisionTreeClassifier().fit(X_train, y_train)
-dt_cv_score = cross_val_score(clf_dt, X_test, y_test, cv=5)
-dt_score = clf_dt.score(X_test, y_test)
+print(players_train_final.toPandas().shape)
 
-# get default random forest classifier
-np.random.seed(42)
-clf_rf = RandomForestClassifier().fit(X_train, y_train)
-rf_cv_score = cross_val_score(clf_rf, X_test, y_test, cv=5)
-rf_score = clf_rf.score(X_test, y_test)
+# build the model and make the prediction
+clf_dt= DecisionTreeClassifier(featuresCol="features", labelCol="label")
+clf_dt = clf_dt.fit(players_train_final)
+pred_clf_dt = clf_dt.transform(players_test_final)
 
-# get default gradient boosting classifier
-gbc = GradientBoostingClassifier().fit(X_train, y_train)
-gbc_cv_score = cross_val_score(gbc, X_test, y_test, cv=5)
-gbc_score = gbc.score(X_test, y_test)
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+# evaluate the model
+evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+accuracy = evaluator.evaluate(pred_clf_dt)
+print(accuracy)
 
-# get default ada boost classifier
-ada = AdaBoostClassifier().fit(X_train, y_train)
-ada_cv_score = cross_val_score(ada, X_test, y_test, cv=5)
-ada_score = ada.score(X_test, y_test)
+from sklearn.metrics import confusion_matrix
 
-print('Decision Tree: ',dt_score, '\nRandomForest: ', rf_score, '\nGradientBoosting: ', gbc_score, '\nAdaBoost: ', ada_score)
+y_p=pred_clf_dt.select("prediction").collect()
+y =pred_clf_dt.select("label").collect()
+
+cm = confusion_matrix(y, y_p)
+tn, fp, fn, tp = cm.ravel()
+print("Confusion Matrix:")
+print(cm)
+
+acc = (tn+tp)/(tp+tn+fp+fn)
+precision = tp/(tp+fp)
+recall = tp/(tp+fn)
 
 import json
+# write the metrics into the scores.json file
 with open('/project/MSIN0166_Data_Engineering_individual/scores.json', "w") as fd:
-    json.dump({"Decision_Tree_score": dt_score, 'Random_Forest_score':rf_score, 'Gradient_Boosting_score':gbc_score, 'Ada_Boost_score':ada_score}, fd, indent=4)
-
-print(classification_report(y_test, ada.predict(X_test)))
-
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.metrics import accuracy_score
-
-# randomised search
-params_rand ={
-    'n_estimators' : np.random.randint(5,200,size = 1),
-    'learning_rate': [0.01,0.02,0.03,0.04,0.05]
-}
-
-rand_ada = RandomizedSearchCV(AdaBoostClassifier(), 
-                            params_rand, 
-                            n_iter=20,
-                            scoring='accuracy', 
-                            n_jobs=1, 
-                            cv=RepeatedStratifiedKFold(n_splits=5), 
-                            random_state=1)
-
-# train the search
-final_ada = rand_ada.fit(X_train, y_train)
-
-# show the best params
-final_ada.best_params_
-
-print(classification_report(y_test,final_ada.predict(X_test)))
+    json.dump({"Accuract":acc , 'Precision':precision, 'Recall': recall, 'F1_Score':2*((precision * recall)/(precision + recall))}, fd, indent=4)
